2f3ab8d0541d23a6d10c21429b24964db8c1173c
Endorithic <endorithic@outlook.com>
2026-07-28 05:47:18 +0200

    Added a function dispatch class

---
+++ include/function_map.hpp
@@ -0,0 +1,64 @@
+#ifndef ENDO_FUNCTION_MAP_HPP
+#define ENDO_FUNCTION_MAP_HPP
+
+#include <type_traits>
+#include <unordered_map>
+#include <string>
+#include <expected>
+
+template <typename Ty>
+concept NotVoid = !std::is_void_v<Ty>;
+
+namespace Endo {
+
+enum class ExecuteError {
+    /// Indicates that a function was not found in the map.
+    FunctionNotFound = 0,
+};
+
+/// HashMap based function lookup and executor.
+/// NOTE: Template type `Ret` cannot be `void`
+template <NotVoid Ret, typename... Args>
+class FunctionMap {
+    using FunctionPointer = Ret(*)(Args...);
+
+private:
+    /// Maps function names as `std::string` to function pointers
+    std::unordered_map<std::string, FunctionPointer> functions;
+
+    /// Default constructor; should not be used by the user.
+    FunctionMap()
+        : functions()
+    { }
+
+public:
+    /// Creates a new empty function map.
+    /// Note: Only one function map can be created per program. This is enforced
+    /// using a singleton pattern.
+    static FunctionMap& create() {
+        static FunctionMap map;
+        return map;
+    }
+
+    /// Executes the function. Returns an expected type containing a possible error
+    /// describing how the function execution failed.
+    [[nodiscard]] std::expected<Ret, ExecuteError> execute(const std::string& name, Args... args) {
+        if (this->functions.contains(name)) {
+            Ret result = (*this->functions[name])(std::forward<Args>(args)...);
+            return result;
+        }
+
+        return std::unexpected(ExecuteError::FunctionNotFound);
+    }
+
+    /// Inserts a function into the function map.
+    void insert(const std::string& name, FunctionPointer pointer) {
+        if (!this->functions.contains(name)) {
+            this->functions[name] = pointer;
+        }
+    }
+};
+
+} // namespace Endo
+
+#endif // ENDO_FUNCTION_MAP_HPP
